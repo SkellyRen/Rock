5e5019d2caba2b415ee6eac77861189f083e9241
Daniel Hazelbaker <daniel@triumph.tech>
2023-04-24 15:00:37 -0700

    - Added support to bold export header row.

---
+++ Rock.JavaScript.Obsidian/Framework/Libs/xlsx-js-style.shim.d.ts
@@ -0,0 +1,6 @@
+declare module "xlsx-js-style" {
+    import xlsx from "xlsx";
+
+    export * from "xlsx";
+    export default xlsx;
+}
--- Rock.JavaScript.Obsidian/Framework/Libs/xlsx.ts
+++ Rock.JavaScript.Obsidian/Framework/Libs/xlsx.ts
@@ -1,1 +1,10 @@
-export * from "xlsx";
+import { type ColInfo, type WorkBook, type WorkSheet } from "xlsx-js-style";
+import xlsx from "xlsx-js-style";
+
+export default xlsx;
+
+export {
+    ColInfo,
+    WorkBook,
+    WorkSheet
+};
